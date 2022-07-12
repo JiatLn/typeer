4f7245f5520dc29ef5fe0a281a183d8b576426fe
林佳涛 <lne128gene980@163.com>
2022-07-12 10:31:36 +0800

    feat: add module [is]

---
+++ src/types/is.ts
@@ -0,0 +1,9 @@
+export type IsAny<T> = 0 extends 1 & T ? true : false
+
+export type IsNumber<T> = T extends number ? true : false
+
+export type IsNever<T> = [T] extends [never] ? true : false
+
+export type IsEqual<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false
